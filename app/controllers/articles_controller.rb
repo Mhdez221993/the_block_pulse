77d9c9526d2b0c2a9eaecfeda0e5febd444c0afe
Moises Hernandez <mhdezcoronado@gmail.com>
2023-07-03 22:51:06 +0200

    Display articles and add test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
-  def index; end
+  def index
+    @articles = Article.all
+  end
 end
 
 # redirect_to '/', status: :unprocessable_entity
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -4,6 +4,7 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   test 'shuld get index' do
     get articles_url
     assert_response :success
-    assert_select 'h1', 'Hello, Rails!'
+    assert_select 'h1', 'Articles'
+    assert_select 'li', Article.count
   end
 end
