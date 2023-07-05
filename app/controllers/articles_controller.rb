e2c8e84d3a57f787cf44adcbb624f2a9aa10d312
Moises Hernandez <mhdezcoronado@gmail.com>
2023-07-05 14:23:59 +0200

    Add create acction and test it

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,19 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def create
+    @article = Article.new(title: 'Stateless', body: 'Code')
+
+    if @article.save
+      redirect_to @article, notice: 'Article was succefully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
 
 # redirect_to '/', status: :unprocessable_entity
+
+# curl -v -X  POST http://127.0.0.1:3000/articles
+# skip_before_action :verify_authenticity_token
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -5,18 +5,30 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     @article = articles(:one)
   end
 
-  test 'shuld get index' do
+  test 'should get index' do
     get articles_url
     assert_response :success
     assert_select 'h1', 'Articles'
     assert_select 'li', Article.count
   end
 
-  test 'shuld get show' do
+  test 'should get show' do
     # get "/articles/#{@article.id}"
     get article_url(@article)
     assert_response :success
     assert_select 'h1', @article.title
     assert_select 'p', @article.body
+  end
+
+  test 'should create article' do
+    assert_difference('Article.count') do
+      post articles_url
+    end
+
+    article = Article.last
+    assert_redirected_to article_path(article)
+    assert_equal 'Stateless', article.title
+    assert_equal 'Code', article.body
+    assert_equal 'Article was succefully created.', flash[:notice]
   end
 end
