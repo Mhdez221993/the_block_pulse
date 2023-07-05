28b487a7afb381132c56f135342004d6eabb62d1
Moises Hernandez <mhdezcoronado@gmail.com>
2023-07-05 16:00:32 +0200

    Test creating a new article

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,26 @@
-require "test_helper"
+require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @article = articles(:one)
+    @valid_title = 'I have a valid tittle'
+    @valid_body = 'This body is long enough.'
+    @title_blanck = "Title can't be blanck"
+    @body_blanck = "Body can't be blanck"
+    @body_short = 'Body is too short (minimun is 10 characters)'
+  end
+
+  test 'should be valid and creatable with correct attributes' do
+    # new
+    article = Article.new(title: @valid_title, body: @valid_body)
+    assert article.valid?
+    assert article.save
+    article.reload
+    assert_not_nil article.id
+    assert_equal @valid_title, article.title
+    assert_equal @valid_body, article.body
+
+    # existing
+    assert @article.valid?
+  end
 end
