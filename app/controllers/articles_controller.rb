b0b61212cf49574a0aeffd1f9bdd921add24f146
Moises Hernandez <mhdezcoronado@gmail.com>
2023-07-06 00:59:05 +0200

    Add edit and update controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,9 +15,23 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to @article, notice: 'Article was succefully created.'
+      redirect_to @article, notice: 'Article was successfully created.'
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
