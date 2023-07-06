adc1a505c1fd7bb3b18c0f65558f30c171bd3ca9
Moises Hernandez <mhdezcoronado@gmail.com>
2023-07-06 20:41:52 +0200

    Add association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true, length: { minimum: 18, maximun: 70 }
   validates :body, presence: true, length: { minimum: 2000, maximun: 8000 }
 end
