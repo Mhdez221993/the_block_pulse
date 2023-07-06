fd2bd9c8510a18070344ebfa221b34def6312dd0
Moises Hernandez <mhdezcoronado@gmail.com>
2023-07-06 20:52:28 +0200

    Add dependent destroy

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 18, maximun: 70 }
   validates :body, presence: true, length: { minimum: 2000, maximun: 8000 }
