e86732e4e4d1d1d3a00387438b504378b851448b
Moises Hernandez <mhdezcoronado@gmail.com>
2023-07-06 16:59:27 +0200

    Test system destoy metho

---
+++ test/system/articles_test.rb
@@ -0,0 +1,19 @@
+require 'application_system_test_case'
+
+class ArticlesTest < ApplicationSystemTestCase
+  setup do
+    @article = articles(:one)
+  end
+
+  test 'destroy an article' do
+    visit article_url(@article)
+    page.accept_confirm do
+      click_on 'Destroy'
+    end
+
+    assert_text 'Article'
+    assert_no_text @article.title
+    assert_text articles(:two).title
+    assert_text 'New Article'
+  end
+end
