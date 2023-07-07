0a50fb8b7f515be081ba823dfc53d64c179fcd75
Moises Hernandez <mhdezcoronado@gmail.com>
2023-07-07 13:20:50 +0200

    Add status on Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,12 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 18, maximun: 70 }
   validates :body, presence: true, length: { minimum: 2000, maximun: 8000 }
+
+  VALID_STATUSES = %w[public private archived].freeze
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
 end
