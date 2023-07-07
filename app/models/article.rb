e75a6d1b0ef3f2ccdff1d2e80389cce9c2df4e6f
Moises Hernandez <mhdezcoronado@gmail.com>
2023-07-07 16:15:41 +0200

    Add visible concern

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,8 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 18, maximun: 70 }
-  validates :body, presence: true, length: { minimum: 2000, maximun: 8000 }
-
-  VALID_STATUSES = %w[public private archived].freeze
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
+  validates :body, presence: true, length: { minimum: 2000, maximun: 10000 }
 end
